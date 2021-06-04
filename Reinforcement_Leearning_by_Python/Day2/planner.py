5db3cbe29196bf765e005d33984197db94cc85f8
手嶋優風 <yteshima@tejimayuufuunoMacBook-Pro.local>
2021-06-04 13:02:42 +0900

    modify

--- Reinforcement_Leearning_by_Python/Day2/planner.py
+++ Reinforcement_Leearning_by_Python/Day2/planner.py
@@ -1,57 +1,66 @@
 
-def V(s, gamma=0.99):
-    V = R(s) + gamma * max_V_on_next_state(s)
-
-    return V
-
-
-def R(s):
-    if s == "happy_end":
-        return 1
-    elif s == "bad_end":
-        return -1
-    else:
-        return 0
-
-
-def max_V_on_next_state(s):
-    if s in ["happy_end", "bad_end"]:
-        return 0
-    actions = ["up", "down"]
-    values = []
-    for a in actions:
-        transition_probs = transit_func(s, a)
-        v = 0
+class Planner():
+    def __init__(self, env):
+        self.env = env
+        self.log = []
+    
+    def initialize(self):
+        self.env.reset()
+        self.log = []
+    
+    def plan(self, gamma=0.9, threshold=0.0001):
+        raise Exception("Planner have to implements plan method.")
+    
+    def transitions_at(self, state, action):
+        transition_probs = self.env.transit_func(state, action)
         for next_state in transition_probs:
             prob = transition_probs[next_state]
-            v += prob * V(next_state)
-            values.append(v)
-    return max(values)
+            reward, _ = self.env.reward_func(next_state)
+            yield prob, next_state, reward
+    
+    def dict_to_grid(self, state_reward_dict):
+        grid = []
+        for i in range(self.env.row_length):
+            row = [0] * self.env.column_length
+            grid.append(row)
+        
+        for s in state_reward_dict:
+            grid[s.row][s.column] = state_reward_dict[s]
+        
+        return grid
+
+class CalueIterationPlanner(Planner):
+    def __init__(self, env):
+        super().__init__(env)
+    
+    def plan(self, gamma=0.9, threshold=0.0001):
+        self.initialize()
+        actions = self.env.actions
+        V = {}
+        for s in self.env.states:
+            V[s] = 0
+        
+        while True:
+            delta = 0
+            self.log.append(self.dict_to_grid(V))
+            for s in V:
+                if not self.env.can_actions_at(s):
+                    continue
+                expect_rewards = []
+                for a in actions:
+                    r = 0
+                    for prob, next_state, reward in self.transitions_at(s, a):
+                        r += prob * (reward + gamma * V[next_state])
+                    expect_rewards.append(r)
+                max_reward = max(expect_rewards)
+                delta = max(delta, abs(max_reward - V[s]))
+                V[s] = max_reward
+            
+            if delta < threshold:
+                break
+        
+        V_grid = self.dict_to_grid(V)
+        
+        return V_grid
 
 
-def transit_func(s, a):
-
-    actions = s.split("_")[1:]
-    LIMIT_GAME_COUNT = 5
-    HAPPY_END_BORDER = 4
-    MOVE_PROB = 0.9
-
-    def next_state(state, action):
-        return "_".join([state, action])
-
-    if len(actions) == LIMIT_GAME_COUNT:
-        up_count = sum([1 if a == "up" else 0 for a in actions])
-        state = "happy_end" if up_count >= HAPPY_END_BORDER else "bad_end"
-        prob = 1.0
-        return {state: prob}
-    else:
-        opposite = "up" if a == "down" else "down"
-        return {
-            next_state(s, a): MOVE_PROB,
-            next_state(s, opposite): 1 - MOVE_PROB
-        }
-
-if __name__ == "__main__":
-    print(V("state"))
-    print(V("state_up_up"))
-    print(V("state_down_down"))
